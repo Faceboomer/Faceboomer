804a685c29ed4000e1b4abb7569d41caec644941
Mariana Simões <marianalimoes@users.noreply.github.com>
2020-12-13 18:07:09 +0000

    DESAFIO DONE + login arranjado

--- js/DESAFIO.js
+++ js/DESAFIO.js
@@ -8,24 +8,8 @@ $("#publishDesafio").on("click", function () {
 });
 
 /* desafio enviar msg */
-// se licar em enviar em GaleriadeImagens.html
+// se clicar em enviar em GaleriadeImagens.html
 $("#publishDesafio2").on("click", function () {
-
   let desafio2 = true;
   localStorage.setItem("desafio2", desafio2);
 });
-
-
-/* verificar o estado dos desafios */
-let desafio1 = localStorage.getItem("desafio1");
-// tornar string da consoleLog em boolean (true)
-desafio1 = JSON.parse(desafio1)
-
-let desafio2 = localStorage.getItem("desafio2");
-// tornar string da consoleLog em boolean (true)
-desafio2 = JSON.parse(desafio2)
-
-if (desafio1 && desafio2) {
-  setInterval(window.location.replace("Final.html"), 3000);
-}
-
