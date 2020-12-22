cd7bfcba9c9b0ca1ae1a32487dd5453367aed4cb
Mariana Simões <marianalimoes@users.noreply.github.com>
2020-12-22 13:25:38 +0000

    Bugs (mensagens + alertas)
    Conversa Nuno escreve msg por cima da foto
    alertas

--- js/menos-alertas.js
+++ js/menos-alertas.js
@@ -1,12 +1,50 @@
+// gerar nr int. aleatório
+// créditos: https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-  if (performance.navigation.type == performance.navigation.TYPE_NAVIGATE) {
+var i = getRandomInt(0, 1);
+var ii = getRandomInt(0, 1);
+var iii = getRandomInt(0, 1);
+
+switch(i) {
+  case 1:
     document.getElementById('aparecer').remove();
-  };
+    console.log("i");
+    break;
+}
 
-  if (performance.navigation.type == performance.navigation.TYPE_NAVIGATE) {
+switch(ii) {
+  case 1:
     document.getElementById('aparecer1').remove();
-  };
+    console.log("ii");
+    break;
+}
 
-  if (performance.navigation.type == performance.navigation.TYPE_NAVIGATE) {
+switch(iii) {
+  case 1:
     document.getElementById('aparecer2').remove();
-  };
+    console.log("iii");
+    break;
+}
+
+
+
+// NÃO FUNCIONA PQ A FORMA COMO CHEGAMOS AO FEED É SEMPRE POR TYPE_NAVIGATE
+// QUE É SEMPRE QUE SE CHEGA À PáG ATRAVÉS DE UM LINK
+/*
+if (performance.navigation.type == performance.navigation.TYPE_NAVIGATE) {
+  document.getElementById('aparecer').remove();
+};
+
+if (performance.navigation.type == performance.navigation.TYPE_NAVIGATE) {
+  document.getElementById('aparecer1').remove();
+};
+
+if (performance.navigation.type == performance.navigation.TYPE_NAVIGATE) {
+  document.getElementById('aparecer2').remove();
+};
+*/
